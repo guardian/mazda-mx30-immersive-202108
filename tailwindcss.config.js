2d634891b59a6fb8e14aab2188b6636b5e2b3fbf
Zu <zu@netmek.biz>
2021-08-10 13:39:48 +1000

    initial

---
+++ tailwindcss.config.js
@@ -0,0 +1,12 @@
+export default {
+    mode: 'jit', // Just-In-Time Compiler
+    purge: ['./src/**/*.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
